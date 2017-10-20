48a71f1bb5cedd785a222ebe3639b76425ef4a2b
Johannes Löthberg <johannes@kyriasis.com>
2017-10-20 23:39:22 +0200

    Start working on doc comments
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- busypenguin/classes.py
+++ busypenguin/classes.py
@@ -4,16 +4,37 @@ from slackclient import SlackClient
 
 
 class Notifier:
+    '''
+    The Notifier class is the base busypenguin class.  It takes a Slack bot
+    access token, and a channel ID that all messages will be posted to.
+    '''
     def __init__(self, access_token, channel):
         self.client = SlackClient(access_token)
         self.channel = channel
 
     def task(self, *args, **kwargs):
+        '''Creates a :py:class:`Task` object, and passes along all arguments.
 
+        Returns:
+            A :py:class:`Task` object.'''
         return Task(self, *args, **kwargs)
 
 
 class Task:
+    '''
+    The Task class represents a single task step.  It may contain an arbitrary
+    number of :py:class:`Subtask` instances whose statuses will be displayed in
+    a table in slack.
+
+    :param Notifier notifier: The top-level Notifier instance this class belongs to.
+                              Should not be passed to the :py:meth:`Notifier.task` method.
+    :param color color: The initial color line while the task is active.
+    :type color: good, warning, or danger
+    :param str title: Title of task message.
+    :param str text: Body text of task message.
+    :param bool status_prefix: Whether to add "Started"/"Finished" to body text automatically.
+    '''
+
     def __init__(self,
                  notifier,
                  color='warning',
@@ -59,16 +80,25 @@ class Task:
         self.message.publish()
 
     def publish(self):
+        '''Trigger message to be published or updated on Slack.'''
         self.message.publish()
 
     def subtask(self, *args, **kwargs):
+        '''Create a :py:class:`Subtask` and passes all arguments to it.'''
         return Subtask(self, *args, **kwargs)
 
     def update(self, *args, **kwargs):
+        '''Convenience method for updating the associated :py:class:`Message` instance.
+        Passes all arguments to :py:meth:`Message.update`.
+        '''
         self.message.update(*args, **kwargs)
 
 
 class Subtask:
+    '''
+    Represents a subtask belonging to a specific top-level :py:class:`Task` step.
+    Subtasks are either in a list or a table field inside the task message.
+    '''
     def __init__(self, task, text, short=False):
         self.task = task
         self.text = text
@@ -97,6 +127,7 @@ class Subtask:
 
 
 class Message:
+    '''Represets a single Slack message.'''
     def __init__(self, notifier, color=None, title=None, text=None, callback_id=None, actions=None):
         self.notifier = notifier
         self.main = {'color': color,
@@ -109,6 +140,7 @@ class Message:
         self.ts = None
 
     def update(self, color=None, title=None, text=None, actions=None):
+        '''Updates any of the message properties.'''
         for (k, v) in [('color', color), ('title', title), ('text', text), ('actions', actions)]:
             if v is not None:
                 self.main[k] = v
@@ -123,13 +155,17 @@ class Message:
                 self.main['fields'][index][k] = v
 
     def add_attachment(self, attachment):
+        '''Adds a Slack attachment to the message and returns its index.'''
         self.extra.append(attachment)
         return len(self.extra) - 1
 
     def update_attachment(self, index, attachment):
+        '''Updates a Slack attachment belonging to the message.'''
         self.extra[index] = attachment
 
     def publish(self):
+        '''Publishes or updates an already published Slack message.
+        This is the only :py:class:`Message` method that sends anything to Slack.'''
         attachments = [self.main] + self.extra
         if not self.ts:
             r = self.notifier.client.api_call('chat.postMessage',
